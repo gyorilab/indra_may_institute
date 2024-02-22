b81b5ffb2ad339694d92403ac78c723a081a7752
Tony Wu <ant.wu@northeastern.edu>
2024-02-22 14:07:45 -0500

    parse csv file of msstats results

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import argparse
+import pandas
 
 
 def run_indra_demo():
-	print("yo")
+	args = get_arguments()
+	pandas_df = pandas.read_csv(args["filename"])
+	print(pandas_df)
+
+
+def get_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", "--filename")
+	return vars(parser.parse_args())
+
 
 if __name__ == "__main__":
 	run_indra_demo()
