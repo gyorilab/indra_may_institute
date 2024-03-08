fd627aff46928569d2ecfdca3143296a911c3aa0
Tony Wu <ant.wu@northeastern.edu>
2024-03-08 11:09:31 -0500

    Adjusting script based on small molecule intervention dataset

--- main.py
+++ main.py
@@ -4,36 +4,66 @@ import requests
 from protmapper import uniprot_client
 
 
+DATASET_NAME = "SMALL_MOLECULE_INTERVENTION"
+DATASET_VALUES = ["BREAST_CANCER", "SMALL_MOLECULE_INTERVENTION"]
+LABELS = ["DMSO-DbET6"]
+P_VALUE_CONSTRAINT = 0.05
+
+
+def validate_settings():
+	if DATASET_NAME not in DATASET_VALUES:
+		raise Exception("ADJUST YOUR DATASET NAME")
+
+
 def run_indra_demo():
 	args = get_arguments()
 	filename = get_filename(args)
-	return query_indra(filename)
+	res = query_indra(filename)
+	print(res.json())
+
+
+def search_uniprot(mnemonic_id):
+	protein_id = uniprot_client.get_id_from_mnemonic(mnemonic_id)
+	if protein_id:
+		return uniprot_client.get_hgnc_id(protein_id)
+	else:
+		return None
 
 
 def construct_df(filename):
 	pandas_df = pandas.read_csv(filename)
-	pandas_df[['sp', 'Protein', 'Gene']] = pandas_df['Protein'].str.split('|', expand=True)
-	pandas_df = pandas_df.loc[pandas_df['adj.pvalue'] < 0.01]
-	pandas_df['HGNC'] = pandas_df['Protein'].apply(lambda protein_id: uniprot_client.get_hgnc_id(protein_id))
+	if DATASET_NAME == "BREAST_CANCER":
+		pandas_df[['sp', 'Protein', 'Gene']] = pandas_df['Protein'].str.split('|', expand=True)
+		pandas_df = pandas_df.loc[pandas_df['adj.pvalue'] < P_VALUE_CONSTRAINT]
+		pandas_df['HGNC'] = pandas_df['Protein'].apply(lambda protein_id: uniprot_client.get_hgnc_id(protein_id))
+	elif DATASET_NAME == "SMALL_MOLECULE_INTERVENTION":
+		pandas_df = pandas_df.loc[pandas_df['adj.pvalue'] < P_VALUE_CONSTRAINT]
+		pandas_df = pandas_df.loc[pandas_df['issue'].isnull()]
+		pandas_df = pandas_df.loc[pandas_df['Label'].isin(LABELS)]
+		pandas_df['HGNC'] = pandas_df['Protein'].apply(
+			lambda mnemonic_id: search_uniprot(mnemonic_id)
+		)
+		pandas_df = pandas_df.loc[pandas_df['HGNC'].notnull()]
 	return pandas_df
 
 
 def query_indra(filename):
 	pandas_df = construct_df(filename)
-	print(len(pandas_df))
 	groundings = create_groundings(pandas_df)
 	res = requests.post(
 		'https://discovery.indra.bio/api/indra_subnetwork_relations',
 		json={'nodes': list(groundings.values())}
 	)
-	print(res.json())
 	return res
 
 
 def create_groundings(df):
+	hgnc_ids = set()
 	groundings = {}
 	for index, row in df.iterrows():
-		groundings[index] = 'HGNC', row['HGNC']
+		hgnc_ids.add(row['HGNC'])
+	for index, entry in enumerate(hgnc_ids):
+		groundings[index] = 'HGNC', entry
 	return groundings
 
 
